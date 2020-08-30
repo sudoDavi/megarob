52730240fc96b4951b935b4df0e9d00fce3910c5
Davi Wendt <daviwendt@gmail.com>
2020-08-30 19:46:41 -0300

    Player movement added

---
+++ entity/Actor.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class Actor : KinematicBody2D
+{
+	[Export] public Vector2 speed = new Vector2(150.0f, 350.0f);
+	[Export] public int MaxSlides = 4;
+	[Export] public float FloorMaxAngle = 0.9f;
+	[Export] public bool InfiniteInertia = false;
+	[Export] public bool StopOnSlopes = true;
+	public AnimatedSprite Animation;
+	public RayCast2D PlatformDetector;
+	public object Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity");
+
+	public Vector2 FloorNormal
+	{
+		get { return Vector2.Up; }
+	}
+
+	public Vector2 FloorDetectDistance
+	{
+		get { return new Vector2(0.0f, 20.0f); }
+	}
+
+	public Vector2 velocity = Vector2.Zero;
+}
--- player/Player.cs
+++ player/Player.cs
@@ -1,25 +1,82 @@
 using Godot;
 using System;
 
-public class Player : Node2D
+public class Player : Actor
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Intellisense is working now, for some reason");
+		PlatformDetector = GetNode<RayCast2D>("PlatformDetector");
+		Animation = GetNode<AnimatedSprite>("Animation");
 	}
-
-	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-	//  public override void _Process(float delta)
-	//  {
-	//      
-	//  }
 	public override void _PhysicsProcess(float delta)
 	{
-
+		HandleMovement(delta);
 	}
+
+	private Vector2 CalculateMoveVelocity(
+		Vector2 linearVelocity, Vector2 direction, Vector2 speed, bool isJumpInterrupted
+	)
+	{
+		Vector2 _velocity = linearVelocity;
+		_velocity.x = speed.x * direction.x;
+
+		if (direction.y != 0.0f)
+			_velocity.y = speed.y * direction.y;
+		if (isJumpInterrupted)
+			// Decrease the Y velocity by multiplying it, but don't set it to 0
+			// as to not be too abrupt.
+			_velocity.y *= 0.6f;
+
+		return _velocity;
+	}
+
+	private void ApplyGravity(float delta)
+	{
+		velocity.y += (int)Gravity * delta;
+	}
+
+	private void HandleAnimation(Vector2 lookDirection)
+	{
+		// Animation.Stop();
+		if (lookDirection.x > 0.0f)
+		{
+			Animation.FlipH = false;
+			Animation.Play();
+		}
+		else if (lookDirection.x < 0.0f)
+		{
+			Animation.FlipH = true;
+			Animation.Play();
+		}
+		else
+			Animation.Stop();
+	}
+	private void HandleMovement(float delta)
+	{
+		ApplyGravity(delta);
+
+		float willJump = IsOnFloor() && Input.IsActionJustPressed("jump") ? -1 : 0;
+		Vector2 direction = new Vector2(
+			Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
+			willJump
+		);
+		Vector2 snapVector;
+		if (direction.y == 0.0f)
+			snapVector = Vector2.Down * FloorDetectDistance;
+		else
+			snapVector = Vector2.Zero;
+		HandleAnimation(direction);
+
+		bool isJumpInterrupted = Input.IsActionJustPressed("jump") && velocity.y < 0.0f;
+		velocity = CalculateMoveVelocity(velocity, direction, speed, isJumpInterrupted);
+
+		StopOnSlopes = !PlatformDetector.IsColliding();
+
+		velocity = MoveAndSlideWithSnap(
+			velocity, snapVector, FloorNormal, StopOnSlopes, MaxSlides, FloorMaxAngle, InfiniteInertia
+		);
+	}
+
+
 }
